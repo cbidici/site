713de58e9b3ae5871967506b123c384c04faae3e
cbidici <cbidici@gmail.com>
2024-09-18 00:25:24 +0100

    Adds robots.txt

--- src/main/java/com/cbidici/site/config/SecurityConfig.java
+++ src/main/java/com/cbidici/site/config/SecurityConfig.java
@@ -3,11 +3,6 @@ package com.cbidici.site.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,7 +12,7 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/", "/post/{id}", "/css/**", "/js/**").permitAll()
+            .requestMatchers("/", "/post/{id}", "/css/**", "/js/**", "/robots.txt").permitAll()
             .requestMatchers("/post/new", "/post/edit/**", "/post/update/**", "/post/delete/**").hasRole("ADMIN")
         )
         .formLogin(login -> login
