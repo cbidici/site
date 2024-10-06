cda5b25be338ccd53f3b0244ca75989a6622c9e7
cbidici <cbidici@gmail.com>
2024-10-06 14:55:12 +0100

    Improves authentication error handling

--- src/main/java/com/cbidici/site/view/config/DelegatedAuthenticationEntryPoint.java
+++ src/main/java/com/cbidici/site/view/config/DelegatedAuthenticationEntryPoint.java
@@ -5,7 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Locale;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -14,25 +14,29 @@ import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ViewResolver;
 
 @Component
+@Slf4j
 public class DelegatedAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-  @Autowired
-  @Qualifier("handlerExceptionResolver")
-  private HandlerExceptionResolver resolver;
+  private final HandlerExceptionResolver exceptionResolver;
+  private final ViewResolver viewResolver;
 
-  @Autowired
-  ViewResolver viewResolver;
+  public DelegatedAuthenticationEntryPoint(
+      @Qualifier("handlerExceptionResolver") HandlerExceptionResolver exceptionResolver,
+      ViewResolver viewResolver) {
+    this.exceptionResolver = exceptionResolver;
+    this.viewResolver = viewResolver;
+  }
 
   @Override
   public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
       throws IOException, ServletException {
-    var val = resolver.resolveException(request, response, null, authException);
+    var resolvedModelAndView = exceptionResolver.resolveException(request, response, null, authException);
     try {
-      var resolved = viewResolver.resolveViewName(val.getViewName(), Locale.getDefault());
-      resolved.render(val.getModel(), request, response);
-      //response.sendError(HttpStatus.FORBIDDEN.value(), "Forbidden");
+      var resolvedView = viewResolver.resolveViewName(resolvedModelAndView.getViewName(), Locale.getDefault());
+      resolvedView.render(resolvedModelAndView.getModel(), request, response);
     } catch (Exception e) {
-      throw new RuntimeException(e);
+      log.error("Error occurred : ", e);
+      response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
     }
   }
 }
